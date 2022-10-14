51df5b325eeb3035d3d7b00b18f4777a2ff62a7b
satrio92 <kukuhsatrio920@gmail.com>
2022-10-14 16:03:57 +0700

    create proker page interface

---
+++ src/components/CardProker.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex flex-col gap-4 items-center w-[300px]">
+        <img :src="item.img" alt="" class="rounded-full w-[242px] h-[242px]">
+        <h3 class="text-2xl text-black font-bold">{{ item.title }}</h3>
+        <p class="text-lg text-black font-medium">{{ item.content }}</p>
+        <router-link to="/" class="px-12 py-2 text-white bg-[#00D12E] rounded-full">Details</router-link>
+      </div>
+</template>
+
+<script>
+export default {
+    name: 'CardProker',
+    components: {},
+    props: ['item']
+}
+</script>
--- src/views/Proker.vue
+++ src/views/Proker.vue
@@ -1,17 +1,37 @@
 <template>
   <Navbar />
-  <section class="w-full h-96 flex justify-center items-center">
-    <h1 class="text-3xl text-black font-bold">Halaman Program Kerja</h1>
+  <section class="w-full flex flex-col my-16 gap-24">
+    <div class="flex flex-col gap-6 px-[150px]">
+      <h1 class="text-3xl text-black font-bold">OUR PROGRAM</h1>
+      <p class="text-xl text-black">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex comm</p>
+    </div>
+    <div class="flex justify-center gap-24 px-[72px] flex-wrap">
+      <CardProker :item="proker[0]" v-for="item in 8" :key="item.id"/>
+    </div>
   </section>
 </template>
 
 <script>
+
 import Navbar from "../components/Navbar.vue";
+import CardProker from "../components/CardProker.vue";
 
 export default {
   name: "Proker",
   components: {
     Navbar,
+    CardProker
   },
+  data() {
+    return {
+      proker: [
+        {
+          "title": "PERTEMUAN RUTIN",
+          "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh at tortor enim, ornare. Platea sed id sit ut hendrerit egestas.",
+          "img": "../src/assets/proker-1.png"
+        }
+      ]
+    }
+  }
 };
 </script>
